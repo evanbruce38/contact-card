9c822c71ba7ecec43e3daee4f47d20c612bc88ce
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-10-17 22:24:44 -0400

    web configuring

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -3,15 +3,19 @@ import "./submit";
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import "{ initdb } from './database';
+
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
 window.addEventListener('load', function () {
+    initfb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
-  });
+});
 
-    // Import CSS files
-    import "../css/index.css";
+// Import CSS files
+import "../css/index.css";
+
