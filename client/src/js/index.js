114fb760523f6c64e208637a8cabd6dc9822faf7
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-10-17 22:37:20 -0400

    added form js

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -4,19 +4,20 @@ import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { initDb, getDb, postDb } from './database';
-
-
+import { fetchCards } from './cards';
+import { toggleForm, clearForm } from './form'
+ 
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
 window.addEventListener('load', function () {
-    initfb()
-     // We are temporarily placing getDb() and postDb() function calls here for testing. We will move it to another event listener later.
-  getDb();
-  postDb("Lernantino", "learnantino@test.com", 8186601234, "Bear");
-  getDb();
-})
+    initfb();
+    fetchCards();
+  document.getElementById('logo').src = Logo;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
+});
 
 // Import CSS files
 import "../css/index.css";
