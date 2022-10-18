daecac817a92c6c9a4913f9f14b20ed31c3758fb
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-10-17 22:30:08 -0400

    added more functions to database js

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -3,18 +3,20 @@ import "./submit";
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import "{ initdb } from './database';
+import { initDb, getDb, postDb } from './database';
+
 
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
 window.addEventListener('load', function () {
-    initfb();
-    document.getElementById('logo').src = Logo;
-    document.getElementById('bearThumbnail').src = Bear;
-    document.getElementById('dogThumbnail').src = Dog;
-});
+    initfb()
+     // We are temporarily placing getDb() and postDb() function calls here for testing. We will move it to another event listener later.
+  getDb();
+  postDb("Lernantino", "learnantino@test.com", 8186601234, "Bear");
+  getDb();
+})
 
 // Import CSS files
 import "../css/index.css";
